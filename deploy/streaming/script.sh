dd1a76420b134dac1e30fb5720ce2cc712710a52
Anh Do <anhkk1245@gmail.com>
2025-09-05 17:08:35 +0700

    fix: update deployment script for zma-signaling to use explicit paths and commands

--- deploy/streaming/script.sh
+++ deploy/streaming/script.sh
@@ -2,25 +2,25 @@
 # Script deploy zma-signaling
 
 SERVER="root@94.100.26.6"
-APP_DIR="dhte-nestjs-monorepo"
-APP_PATH="apps/zma-signaling"
-PM2_NAME="zma-signaling"
 
 ssh $SERVER << 'EOF'
   set -e
-  cd ~/$APP_DIR
+
+  cd ~/dhte-nestjs-monorepo
+
+  pwd
 
   echo "🔄 Pulling latest code..."
-  git pull
+  git pull origin main
 
-  cd $APP_PATH
+  cd apps/zma-signaling
 
   echo "🛑 Stopping old process..."
   pm2 stop 0 || true
   pm2 delete 0 || true
 
   echo "🚀 Starting new process..."
-  pm2 start "npx nx serve $PM2_NAME" --name $PM2_NAME
+  pm2 start "npx nx serve zma-signaling" --name zma-signaling
 
   echo "✅ Deployment completed."
 EOF
